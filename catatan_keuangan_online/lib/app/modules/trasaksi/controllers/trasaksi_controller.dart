d92238a73bd13d54621e79dbd4e006b41e68e3b9
Gede Bayu <bayusth@gmail.com>
2022-11-06 22:59:41 +0800

    token

--- catatan_keuangan_online/lib/app/modules/trasaksi/controllers/trasaksi_controller.dart
+++ catatan_keuangan_online/lib/app/modules/trasaksi/controllers/trasaksi_controller.dart
@@ -1,7 +1,11 @@
+import 'package:catatan_keuangan_online/app/mahas/services/http_api.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class TrasaksiController extends GetxController {
-  void showMessage() {
-    Get.defaultDialog(title: 'Hello');
+  void showMessage() async {
+    var token = await FirebaseAuth.instance.currentUser!.getIdToken(true);
+    print(token.substring(0, 400));
+    print(token.substring(400));
   }
 }
